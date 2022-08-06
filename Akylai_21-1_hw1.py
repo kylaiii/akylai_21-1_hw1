55d4648b65290b17010753de307d00a5cbca0b68
Akylai Syd <110735537+kylaiii@users.noreply.github.com>
2022-08-06 22:29:29 +0600

    Add files via upload

---
+++ Akylai_21-1_hw1.py
@@ -0,0 +1,73 @@
+
+class Dog:
+    def __init__(self, name):
+        self.name = name
+    def voice(self):
+        print(f'{self.name}: gaf gaf gaf')
+
+togo = Dog('Togo')
+togo.voice()
+
+class Cow:
+    def __init__(self, name):
+        self.name = name
+    def voice(self):
+        print(f'{self.name}: moo moo moo')
+
+cow = Cow('Cow')
+cow.voice()
+
+class Cat:
+    def __init__(self, name):
+        self.name = name
+    def voice(self):
+        print(f'{self.name}: meow meow')
+
+kitty = Cat('Kitty')
+kitty.voice()
+
+class Bear:
+    def __init__(self, name):
+        self.name = name
+    def voice(self):
+        print(f'{self.name}: grrrr grrrr')
+
+bear = Bear('Bear')
+bear.voice()
+
+
+class Dog:
+    def __init__(self, name):
+        self.name = name
+    def voice(self):
+        print(f'{self.name}: gaf gaf gaf')
+
+togo = Dog('Togo')
+togo.voice()
+
+class Cow:
+    def __init__(self, name):
+        self.name = name
+    def voice(self):
+        print(f'{self.name}: moo moo moo')
+
+cow = Cow('Cow')
+cow.voice()
+
+class Cat:
+    def __init__(self, name):
+        self.name = name
+    def voice(self):
+        print(f'{self.name}: meow meow')
+
+kitty = Cat('Kitty')
+kitty.voice()
+
+class Bear:
+    def __init__(self, name):
+        self.name = name
+    def voice(self):
+        print(f'{self.name}: grrrr grrrr')
+
+bear = Bear('Bear')
+bear.voice()
